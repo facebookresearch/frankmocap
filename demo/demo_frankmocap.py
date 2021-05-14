ded25f239bcd87e206b5ae5cdfbf9230e4a2e372
NickVeld <NickVeld@users.noreply.github.com>
2021-05-14 18:06:29 +0300

    extract common parts and detect_hand_bbox_... into separate file + remove unused imports

--- demo/demo_frankmocap.py
+++ demo/demo_frankmocap.py
@@ -1,31 +1,17 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 
-import os
-import sys
-import os.path as osp
 import torch
-from torchvision.transforms import Normalize
 import numpy as np
-import cv2
-import argparse
-import json
-import pickle
 
-############# input parameters  #############
-from demo.demo_options import DemoOptions
 from bodymocap.body_mocap_api import BodyMocap
 from handmocap.hand_mocap_api import HandMocap
 import mocap_utils.demo_utils as demo_utils
-import mocap_utils.general_utils as gnu
-from mocap_utils.timer import Timer
-from datetime import datetime
 
-from bodymocap.body_bbox_detector import BodyPoseEstimator
 from handmocap.hand_bbox_detector import HandBboxDetector
 from integration.copy_and_paste import integration_copy_paste
 
-import renderer.image_utils as imu
-from renderer.viewer2D import ImShow
+import demo.demo_common
+from demo.demo_options import DemoOptions
 
 
 def __filter_bbox_list(body_bbox_list, hand_bbox_list, single_person):
@@ -108,66 +94,11 @@ def run_regress(
 
 
 def run_frank_mocap(args, bbox_detector, body_mocap, hand_mocap, visualizer):
-    #Setup input data to handle different types of inputs
-    input_type, input_data = demo_utils.setup_input(args)
+    for input_frame_and_metadata in demo.demo_common.input_frame_and_metadata_iterator(args):
+        image_path = input_frame_and_metadata.image_path
+        img_original_bgr = input_frame_and_metadata.img_original_bgr
+        load_bbox = input_frame_and_metadata.load_bbox
 
-    cur_frame = args.start_frame
-    video_frame = 0
-    while True:
-        # load data
-        load_bbox = False
-
-        if input_type =='image_dir':
-            if cur_frame < len(input_data):
-                image_path = input_data[cur_frame]
-                img_original_bgr  = cv2.imread(image_path)
-            else:
-                img_original_bgr = None
-
-        elif input_type == 'bbox_dir':
-            if cur_frame < len(input_data):
-                image_path = input_data[cur_frame]['image_path']
-                hand_bbox_list = input_data[cur_frame]['hand_bbox_list']
-                body_bbox_list = input_data[cur_frame]['body_bbox_list']
-                img_original_bgr  = cv2.imread(image_path)
-                load_bbox = True
-            else:
-                img_original_bgr = None
-
-        elif input_type == 'video':      
-            _, img_original_bgr = input_data.read()
-            if video_frame < cur_frame:
-                video_frame += 1
-                continue
-          # save the obtained video frames
-            image_path = osp.join(args.out_dir, "frames", f"{cur_frame:05d}.jpg")
-            if img_original_bgr is not None:
-                video_frame += 1
-                if args.save_frame:
-                    gnu.make_subdir(image_path)
-                    cv2.imwrite(image_path, img_original_bgr)
-        
-        elif input_type == 'webcam':
-            _, img_original_bgr = input_data.read()
-
-            if video_frame < cur_frame:
-                video_frame += 1
-                continue
-            # save the obtained video frames
-            image_path = osp.join(args.out_dir, "frames", f"scene_{cur_frame:05d}.jpg")
-            if img_original_bgr is not None:
-                video_frame += 1
-                if args.save_frame:
-                    gnu.make_subdir(image_path)
-                    cv2.imwrite(image_path, img_original_bgr)
-        else:
-            assert False, "Unknown input_type"
-
-        cur_frame +=1
-        if img_original_bgr is None or cur_frame > args.end_frame:
-            break   
-        print("--------------------------------------")
-        
         # bbox detection
         if not load_bbox:
             body_bbox_list, hand_bbox_list = list(), list()
@@ -186,39 +117,9 @@ def run_frank_mocap(args, bbox_detector, body_mocap, hand_mocap, visualizer):
             print(f"No body deteced: {image_path}")
             continue
 
-        pred_mesh_list = demo_utils.extract_mesh_from_output(pred_output_list)
-
-        # visualization
-        res_img = visualizer.visualize(
-            img_original_bgr,
-            pred_mesh_list = pred_mesh_list,
-            body_bbox_list = body_bbox_list,
-            hand_bbox_list = hand_bbox_list)
-        
-       # show result in the screen
-        if not args.no_display:
-            res_img = res_img.astype(np.uint8)
-            ImShow(res_img)
-
-        # save result image
-        if args.out_dir is not None:
-            demo_utils.save_res_img(args.out_dir, image_path, res_img)
-
-        # save predictions to pkl
-        if args.save_pred_pkl:
-            demo_type = 'frank'
-            demo_utils.save_pred_to_pkl(
-                args, demo_type, image_path, body_bbox_list, hand_bbox_list, pred_output_list)
-
-        print(f"Processed : {image_path}")
-
-    # save images as a video
-    if not args.no_video_out and input_type in ['video', 'webcam']:
-        demo_utils.gen_video_out(args.out_dir, args.seq_name)
-
-    if input_type =='webcam' and input_data is not None:
-        input_data.release()
-    cv2.destroyAllWindows()
+        demo.demo_common.show_and_save_result(
+            args, 'frank', input_frame_and_metadata, visualizer, pred_output_list
+        )
 
 def main():
     args = DemoOptions().parse()
